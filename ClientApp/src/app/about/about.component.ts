9b6e1164a0ef1d1ba0fc2cd17e04bece2fdef78f
Gerry Garcia <gpgarcia1959@gmail.com>
2018-07-01 20:28:06 -0400

    look update

--- ClientApp/src/app/about/about.component.ts
+++ ClientApp/src/app/about/about.component.ts
@@ -25,34 +25,6 @@ export class AboutComponent implements OnInit {
     );
   }
 
-  deferredPrompt = null;
-  btnDisabled = 0;
-
-  ngOnInit() {
-    window.addEventListener('beforeinstallprompt', (e) => {
-      console.log('beforeinstallprompt');
-      e.preventDefault();
-      this.deferredPrompt = e;
-      // Update UI notify the user they can add to home screen
-      this.btnDisabled = 0; // show
-    });
-  }
-
-  onClick() {
-    // hide our user interface that shows our A2HS button
-    this.btnDisabled = 1; // hide
-    // Show the prompt
-    this.deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    this.deferredPrompt.userChoice
-      .then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
-        this.deferredPrompt = null;
-      });
-  }
+  ngOnInit() {}
 
 }
--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,  OnInit} from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 
@@ -7,9 +7,41 @@ import { RouterModule } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Spammer';
   isCollapsed = true;
+
+
+
+  deferredPrompt = null;
+  btnDisabled = 0;
+
+  ngOnInit() {
+    window.addEventListener('beforeinstallprompt', (e) => {
+      console.log('beforeinstallprompt');
+      e.preventDefault();
+      this.deferredPrompt = e;
+      // Update UI notify the user they can add to home screen
+      this.btnDisabled = 0; // show
+    });
+  }
+
+  onClick() {
+    // hide our user interface that shows our A2HS button
+    this.btnDisabled = 1; // hide
+    // Show the prompt
+    this.deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    this.deferredPrompt.userChoice
+      .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        this.deferredPrompt = null;
+      });
+  }
 
   clearUser() {
     sessionStorage.setItem('currentUser', '');
